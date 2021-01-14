c57c2dd020c4b8d55e66109268f360428a648895
sidens3 <sidens3@gmail.com>
2021-01-14 15:24:53 +0300

    adding timer for eggs hardness timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var eggTime = ["Soft" : 5 * 60, "Medium" : 7 * 60, "Hard" : 12 * 60]
+    
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        guard let hardness = sender.currentTitle else { return }
+        guard let cookingTimeByHardness = eggTime[hardness] else { return }
+        
+        print(cookingTimeByHardness)
+        startTimer(time: cookingTimeByHardness)
+    }
+    
+    func startTimer(time: Int){
+        var seconds = time
 
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            seconds -= 1
+            if seconds == 0 {
+                timer.invalidate()
+            } else {
+                print(seconds)
+            }
+        }
+    }
 }
