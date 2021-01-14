97d663b5c1e9f52371d9276ad3d4d1e47cf5b2e1
sidens3 <sidens3@gmail.com>
2021-01-14 19:11:45 +0300

    complete progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,31 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var eggTime = ["Soft" : 5 * 60, "Medium" : 7 * 60, "Hard" : 12 * 60]
+    @IBOutlet weak var screenTitle: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
+    
+    let eggTime = ["Soft" : 5 * 60, "Medium" : 7 * 60, "Hard" : 12 * 60]
+    
+    var totalTime : Float = 60
+    var secondsPassed : Float = 0
+    var timer = Timer()
+    
+    var persentagesProgress = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        progressBar.progress = 0
+        screenTitle.text = "How do you like your eggs?"
     }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         guard let hardness = sender.currentTitle else { return }
         guard let cookingTimeByHardness = eggTime[hardness] else { return }
         
-        print(cookingTimeByHardness)
-        startTimer(time: cookingTimeByHardness)
+        startTimer(timeToWait: Float(cookingTimeByHardness))
     }
     
-    func startTimer(time: Int){
-        var seconds = time
-
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-            seconds -= 1
-            if seconds == 0 {
-                timer.invalidate()
-            } else {
-                print(seconds)
-            }
+    func startTimer(timeToWait: Float){
+        timer.invalidate()
+        secondsPassed = 0
+        progressBar.progress = 0
+        screenTitle.text = "Eggs in progress"
+        
+        totalTime = timeToWait
+    
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if secondsPassed != totalTime {
+            secondsPassed += 1
+            print("\(secondsPassed)" + "/" + "\(totalTime)" )
+        } else {
+            timer.invalidate()
+            screenTitle.text = "Complete"
         }
+        let persentProgress = secondsPassed / totalTime
+        progressBar.progress = persentProgress
     }
 }
